de7aa965791f08c7be1798c11d5e157fc9a59c12
corentinparp <corentin.parpette@accenture.com>
2024-01-08 12:31:21 +0100

    feat: initialization & initial configuration

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
+
+const withPWA = require("@ducanh2912/next-pwa").default({
+  dest: "public",
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+})
+
 const nextConfig = {}
 
-module.exports = nextConfig
+module.exports = withPWA(nextConfig)
